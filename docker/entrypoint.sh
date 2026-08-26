fae1821b36bf37cbb42b8f9f0b6a6b7bad42a961
Lilith Silver <84940819+LilithSilver@users.noreply.github.com>
2026-08-26 03:37:48 -0700

    fix typo

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 set -- \
     --data-dir "/data" \
     --port "3000" \
-    --auth "${AUTH:-none}"
-    --oidc-redirect-port "${OIDC_REDIRECT_PORT:-58656}"
+    --auth "${AUTH:-none}" \
+    --oidc-redirect-port "${OIDC_REDIRECT_PORT:-58656}" \
     --oidc-client-id "${OIDC_REDIRECT_PORT:-backstitch}"
 
 if [ -n "${WEBVIEWER:-}" ]; then
