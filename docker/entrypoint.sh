e19f102a79398fae636e39d47d1b90a7ead37fca
Lilith Silver <84940819+LilithSilver@users.noreply.github.com>
2026-08-26 03:50:20 -0700

    oops

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,7 +6,7 @@ set -- \
     --port "3000" \
     --auth "${AUTH:-none}" \
     --oidc-redirect-port "${OIDC_REDIRECT_PORT:-58656}" \
-    --oidc-client-id "${OIDC_REDIRECT_PORT:-backstitch}"
+    --oidc-client-id "${OIDC_CLIENT_ID:-backstitch}"
 
 if [ -n "${WEBVIEWER:-}" ]; then
     set -- "$@" --webviewer "$WEBVIEWER"
